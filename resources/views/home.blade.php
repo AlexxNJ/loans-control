e4ae5c5efad3956e884200855d9cd5d0a90ffe6f
Alejandro Nuñez <39980306+AlexxNu@users.noreply.github.com>
2020-11-19 15:39:38 -0600

    Se modifico el texto de las cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
                             <p class="text-inverse-danger my-6">$ {{ $wallet->quantity }} MXN
                             <br /></p>
                         @endforeach
-                        <a href="{{ route('wallets.index') }}" class="btn btn-warning font-weight-bold py-2 px-6">Más información</a>
+                        <a href="{{ route('wallets.index') }}" class="btn btn-warning font-weight-bold py-2 px-6">Ver Más</a>
                     </div>
                 </div>
             </div>
@@ -29,7 +29,7 @@
                         <h4 class="text-inverse-danger mt-2 font-weight-bolder">Prestamos</h4>
                         <p class="text-inverse-danger my-6">$ {{ $loans }} MXN
                         <br /></p>
-                        <a href="{{ route('loans.index') }}" class="btn btn-warning font-weight-bold py-2 px-6">Más información</a>
+                        <a href="{{ route('loans.index') }}" class="btn btn-warning font-weight-bold py-2 px-6">Ver Más</a>
                     </div>
                 </div>
             </div>
@@ -44,7 +44,7 @@
                             <p class="text-inverse-danger my-6">$ {{ $payments }} MXN
                             <br /></p>
                         
-                        <a href="{{ route('payments.index') }}" class="btn btn-success font-weight-bold py-2 px-6">Más información</a>
+                        <a href="{{ route('payments.index') }}" class="btn btn-success font-weight-bold py-2 px-6">Ver Más</a>
                     </div>
                 </div>
             </div>
@@ -58,7 +58,7 @@
                         <h4 class="text-success mt-2 font-weight-bolder">Clientes</h4>
                         <p class="text-success my-6">Total: {{ $totalCustomers }}
                         <br /></p>
-                        <a href="{{ route('customers.index') }}" class="btn btn-success font-weight-bold py-2 px-6">Más información</a>
+                        <a href="{{ route('customers.index') }}" class="btn btn-success font-weight-bold py-2 px-6">Ver Más</a>
                     </div>
                 </div>
             </div>
@@ -72,7 +72,7 @@
                         <h4 class="text-inverse-danger mt-2 font-weight-bolder">Gastos</h4>
                         <p class="text-inverse-danger my-6">$ {{ $expenses }} MXN
                         <br /></p>
-                        <a href="{{ route('expenses.index') }}" class="btn btn-danger font-weight-bold py-2 px-6">Más información</a>
+                        <a href="{{ route('expenses.index') }}" class="btn btn-danger font-weight-bold py-2 px-6">Ver Más</a>
                     </div>
                 </div>
             </div>
@@ -86,7 +86,7 @@
                         <h4 class="text-danger mt-2 font-weight-bolder">Ingresos</h4>
                         <p class="text-dark-50 my-6">$ {{ $incomes }} MXN
                         <br /></p>
-                        <a href="{{ route('incomes.index') }}" class="btn btn-danger font-weight-bold py-2 px-6">Más información</a>
+                        <a href="{{ route('incomes.index') }}" class="btn btn-danger font-weight-bold py-2 px-6">Ver Más</a>
                     </div>
                 </div>
             </div>
